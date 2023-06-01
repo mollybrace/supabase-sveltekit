cc8bf05986d8b8282bf2288db963f2b38b9818f1
mollybrace <molly.rose.brace@gmail.com>
2023-06-01 21:23:27 +0100

    Send to trips page

--- src/routes/logging-in/+page.svelte
+++ src/routes/logging-in/+page.svelte
@@ -10,13 +10,7 @@
 
 		// check if user has been set in session store then redirect
 		if (browser && data.session) {
-			goto(redirectTo ?? '/account');
+			goto(redirectTo ?? '/trips');
 		}
 	}
 </script>
-
-<section>
-	"Because as we know, there are known knowns; there are things we know we know. We also know there
-	are known unknowns; that is to say we know there are some things we do not know. But there are
-	also unknown unknowns—the ones we don't know we don't know" - Donald Rumsfeld
-</section>
